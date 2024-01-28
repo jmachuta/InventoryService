a0948d7c4adf3ab489470771f83d30df2278c481
joshua machuta <meathead-boy@hotmail.com>
2024-01-27 23:34:04 -0500

    Add project files.

---
+++ InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        // GET: api/<InventoryController>
+        [HttpGet]
+        public string Get()
+        {
+            return "App is running";
+        }
+
+        // GET api/<InventoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        // POST api/<InventoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // Load inventory
+        // Add item
+        // Update item
+        // Delete item
+    }
+}
